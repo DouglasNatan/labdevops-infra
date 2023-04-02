d63cd63b3d473cc480c352115dcbe4266f04e46d
DouglasNatan <36136348+DouglasNatan@users.noreply.github.com>
2023-04-02 15:50:21 -0300

    v001

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,9 +1,4 @@
-resource "google_service_account" "sa-dnsg2006" {
-  account_id   = "sa-dnsg2006"
-  display_name = "Service Account User"
-}
-
-resource "google_compute_instance" "myVM" {
+resource "google_compute_instance" "dev-env" {
   name         = var.vm_name
   machine_type = "n1-standard-1"
   zone         = var.zone
